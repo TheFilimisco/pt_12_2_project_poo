049a5c2feb1d4db803d81e99d831bbfd62a5d783
TheFilimisco <54910978@iespoblenou.org>
2025-04-02 22:48:06 +0200

    finish interaction!

--- test/forumapp/models/user/GuestUserTest.java
+++ test/forumapp/models/user/GuestUserTest.java
@@ -1,5 +1,9 @@
 package forumapp.models.user;
 
+import forumapp.controller.serviceauthorization.LoginRequestDTO;
+import forumapp.controller.serviceauthorization.ManagerAuth;
+import forumapp.controller.serviceforum.ManagerForum;
+import forumapp.controller.serviceprofileuser.ManagerProfileUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +13,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GuestUserTest {
     private GuestUser guestUser;
+    private ManagerAuth managerAuth;
+    private ManagerForum managerForum;
+    private ManagerProfileUser managerProfileUser;
+
 
     @BeforeEach
     void setUp() {
         guestUser = new GuestUser();
+        managerAuth = new ManagerAuth();
+        managerForum = new ManagerForum();
     }
 
     @Test
@@ -25,5 +35,17 @@ class GuestUserTest {
         assertEquals(LocalDate.now(),guestUser.getDateSession());
     }
 
+    @Test
+    void loginGuestUser() {
+        // Automatically login like a GuestUser
+        var getUser = managerAuth.login(new LoginRequestDTO("", ""));
+        assertEquals(GuestUser.class, getUser.getClass());
+    }
 
+    @Test
+    void accessProfileUser(){
+        //Is null by auth register, it instead add ProfileUser just access to Forum, but not rise towards ProfileUser
+        var accessProfileUser = managerForum.getProfileUser(guestUser.getUserId());
+        assertNull(accessProfileUser);
+    }
 }
